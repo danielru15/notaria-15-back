1316fe46390770211092d535a864e423f0bb43d1
Davidvera27 <davidrestrepovera@gmail.com>
2025-03-04 10:37:53 -0500

    Update conection.database.ts
    Configuración de credenciales para conexión con DB PgSQL

--- src/config/conection.database.ts
+++ src/config/conection.database.ts
@@ -1,7 +1,7 @@
 import pg from 'pg'
 const { Pool } = pg
 
-const connectionString:string = 'postgresql://postgres:9715@localhost:5432/notaria15'
+const connectionString:string = 'postgresql://postgres:password@localhost:5432/notaria15'
 
 export const db = new Pool({
     allowExitOnIdle: true,
