5617f0a0353aa6a0e68cf0cb75e9fbdbd93665b1
FelipeR96 <felipereinaleso96@gmail.com>
2025-03-13 19:37:10 -0500

    Update conection.database.ts

--- src/config/conection.database.ts
+++ src/config/conection.database.ts
@@ -1,7 +1,7 @@
 import pg from 'pg'
 const { Pool } = pg
 
-const connectionString:string = 'postgresql://postgres:9715@localhost:5432/notaria15'
+const connectionString:string = 'postgresql://postgres:criminalset1996@localhost:5432/notaria15'
 
 export const db = new Pool({
     allowExitOnIdle: true,
